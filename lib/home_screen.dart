90bbe58826309761e4f22f2acd3c97b7bc6c4e19
tanvirhridoy <ikramulhasantanvir@gmail.com>
2024-12-09 02:03:09 +0600

    completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,9 +10,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late GoogleMapController _googleMapController;
+  @override
+  void initState() {
+    super.initState();
+    _listenCurrentLocation();
+  }
 
-  Position? position;
+  late GoogleMapController _googleMapController;
+  Position? _position;
+
+  LatLng? _latLng;
+  final Set<Marker> _marker = {};
 
   Future<void> _getCurrentLocation() async {
     final isGranted = await _isLocationPermissionGranted();
@@ -20,8 +28,18 @@ class _HomeScreenState extends State<HomeScreen> {
       final isEnable = await _checkGPSServiceEnable();
       if (isEnable) {
         Position currentPosition = await Geolocator.getCurrentPosition(
-            locationSettings: const LocationSettings(distanceFilter: 10));
-        position = currentPosition;
+          locationSettings: const LocationSettings(
+              timeLimit: Duration(seconds: 10),
+              accuracy: LocationAccuracy.bestForNavigation),
+        );
+        _position = currentPosition;
+        _marker.add(
+          Marker(
+            markerId: const MarkerId('current-location'),
+            position: LatLng(_position!.latitude, _position!.longitude),
+            infoWindow: const InfoWindow(title: 'My current location'),
+          ),
+        );
         setState(() {});
       } else {
         Geolocator.openLocationSettings();
@@ -72,85 +90,87 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.amber,
       ),
       body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(23.864765120989297, 90.29022071510553),
-          zoom: 16,
-        ),
-        onTap: (LatLng? latLog) {
-          debugPrint(latLog.toString());
+        onTap: (LatLng latLng) {
+          _marker.add(
+            Marker(
+                draggable: true,
+                icon: BitmapDescriptor.defaultMarkerWithHue(
+                    BitmapDescriptor.hueBlue),
+                markerId: const MarkerId('tap-marker'),
+                position: latLng,
+                infoWindow: InfoWindow(
+                    title: '${latLng.latitude},${latLng.longitude}')),
+          );
+          _latLng = latLng;
+          setState(() {});
         },
-        zoomControlsEnabled: true,
-        trafficEnabled: true,
         onMapCreated: (GoogleMapController controller) {
           _googleMapController = controller;
         },
-        markers: <Marker>{
-          const Marker(
-            markerId: MarkerId('initial-value'),
-            position: LatLng(23.864765120989297, 90.29022071510553),
-          ),
-          Marker(
-            markerId: const MarkerId('home'),
-            position: const LatLng(23.863986939932396, 90.28948478400709),
-            infoWindow: const InfoWindow(title: 'home'),
-            icon:
-                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-          ),
-        },
-        circles: <Circle>{
-          Circle(
-              circleId: const CircleId('initial-circle'),
-              center: const LatLng(23.862241994490237, 90.28771318495274),
-              radius: 300,
-              fillColor: Colors.red.withOpacity(0.3),
-              strokeWidth: 0),
-        },
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(0, 0),
+        ),
+        trafficEnabled: true,
+        markers: _marker,
         polylines: <Polyline>{
-          const Polyline(
-            width: 3,
-            jointType: JointType.mitered,
-            polylineId: PolylineId('initial-polyline'),
-            points: <LatLng>[
-              LatLng(23.86998292187493, 90.28878439217806),
-              LatLng(23.86686600252344, 90.2925730124116),
-              LatLng(23.861674138610514, 90.29284290969372),
-              LatLng(23.85739612920099, 90.29078297317028),
-            ],
-          ),
-        },
-        polygons: <Polygon>{
-          const Polygon(
-            polygonId: PolygonId('initial-polygon'),
-            points: <LatLng>[
-              LatLng(23.856226643038394, 90.28297506272793),
-              LatLng(23.85491456144058, 90.28883635997772),
-              LatLng(23.851763242247664, 90.28615079820156),
-              LatLng(23.847546542615284, 90.28483886271715),
-              LatLng(23.85378184932356, 90.2809888869524),
-            ],
-          ),
+          if (_position != null && _latLng != null)
+            Polyline(
+              color: Colors.blue,
+              width: 3,
+              jointType: JointType.mitered,
+              polylineId: const PolylineId('initial-polyline'),
+              points: <LatLng>[
+                LatLng(_position!.latitude, _position!.longitude),
+                LatLng(_latLng!.latitude, _latLng!.longitude),
+              ],
+            ),
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          if (position == null) {
-            await _getCurrentLocation();
-          }
-          if (position != null) {
-            _googleMapController.animateCamera(
-              CameraUpdate.newCameraPosition(
-                CameraPosition(
-                  zoom: 16,
-                  target: LatLng(position!.latitude, position!.longitude),
-                ),
-              ),
-            );
-          }
-        },
-        backgroundColor: Colors.amber,
-        child: const Icon(Icons.my_location),
-      ),
+      floatingActionButton: _buildFloatingActionButton(),
     );
+  }
+
+  FloatingActionButton _buildFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () async {
+        if (_position == null) {
+          await _getCurrentLocation();
+        }
+        if (_position != null) {
+          _googleMapController.animateCamera(
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                zoom: 16,
+                target: LatLng(_position!.latitude, _position!.longitude),
+              ),
+            ),
+          );
+        }
+      },
+      backgroundColor: Colors.amber,
+      child: const Icon(Icons.my_location),
+    );
+  }
+
+  Future<void> _listenCurrentLocation() async {
+    final isGranted = await _isLocationPermissionGranted();
+    if (isGranted) {
+      final isEnable = await _checkGPSServiceEnable();
+      if (isEnable) {
+        Geolocator.getPositionStream().listen((pos) {
+          print(pos);
+        });
+      } else {
+        Geolocator.openLocationSettings();
+      }
+    } else {
+      final result = await _requestLocationPermission();
+      if (result) {
+        _getCurrentLocation();
+      } else {
+        Geolocator.openAppSettings();
+      }
+    }
   }
 }
